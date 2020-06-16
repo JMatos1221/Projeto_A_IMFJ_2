23deaf4fbc4393fd2ebc0801c26c3792b6e1344c
João Matos <joao.matos1221@gmail.com>
2020-06-16 02:49:19 +0100

    Menu Created

--- IMFJ_2_Projeto_A_Joao_Matos.py
+++ IMFJ_2_Projeto_A_Joao_Matos.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import math
+
+print("What problem do you want to solve?")
+print("1. Floatation")
+print("2. Springs")
+
+def main():
+    while(True):
+        userIn = input().replace(" ", "")
+
+        if (userIn == "1"):
+            program = 1
+            break
+        
+        elif (userIn == "2"):
+            program = 2
+            break
+
+        else:
+            print("Please choose 1 or 2")
+
